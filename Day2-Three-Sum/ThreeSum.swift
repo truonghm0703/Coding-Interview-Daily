fe8b362bd6499fd66947e4bc4c07b69f78593486
Hoang Minh Truong <truonghm@sendo.vn>
2020-07-21 13:58:14 +0700

    Day2: Three Sum

---
+++ Day2-Three-Sum/ThreeSum.swift
@@ -0,0 +1,33 @@
+
+//## 3Sum
+//
+//> Given an array of integers, find all subsets of the array with 3 values where the 3 values sum up to a target number.
+//>
+//> **Note**: The solution subsets must not contain duplicate triplets.
+//>
+//> For example, given the array [-1, 0, 1, 2, -1, -4], and the target **0**:
+//> The solution set is: [[-1, 0, 1], [-1, -1, 2]] // The two **-1** values in the array are considered to be distinct
+//
+//There are 2 key procedures in solving this algorithm. Sorting the array, and avoiding duplicates.
+
+func threeSum(_ nums: [Int], target: Int) -> Set<[Int]> {
+    let sortedNums = nums.sorted()
+    var dataReturn: Set<[Int]> = []
+    for i in 0...sortedNums.count - 3 {
+        var leftIndex = i + 1
+        var rightIndex = sortedNums.count - 1
+        while leftIndex < rightIndex {
+            let sum = sortedNums[i] + sortedNums[leftIndex] + sortedNums[rightIndex]
+            if sum == target {
+                dataReturn.insert([sortedNums[i], sortedNums[leftIndex], sortedNums[rightIndex]])
+                rightIndex -= 1
+                leftIndex += 1
+            } else if sum > target {
+                rightIndex -= 1
+            } else {
+                leftIndex += 1
+            }
+        }
+    }
+    return dataReturn
+}
--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # Coding-Interview-Daily
 
-Day 1. Two Sum
+## Day 1. Two Sum
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+>Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+>
+>You may assume that each input would have exactly one solution, and you may not use the same element twice.
+>
+>Example:
+>
+>Given nums = [2, 7, 11, 15], target = 9,
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+>Because nums[0] + nums[1] = 2 + 7 = 9,
+>return [0, 1].
 
-Example:
+## Day 2. Three Sum
 
-Given nums = [2, 7, 11, 15], target = 9,
+> Given an array of integers, find all subsets of the array with 3 values where the 3 values sum up to a target number. 
+>
+> **Note**: The solution subsets must not contain duplicate triplets.
+>
+> For example, given the array [-1, 0, 1, 2, -1, -4], and the target **0**:
+> The solution set is: [[-1, 0, 1], [-1, -1, 2]] // The two **-1** values in the array are considered to be distinct
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+>There are 2 key procedures in solving this algorithm. Sorting the array, and avoiding duplicates.
