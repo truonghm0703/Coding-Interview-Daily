4eddbacbb963953d1bcf017a4c83fd476b082ca4
truonghm0703 <minhtruong0703@gmail.com>
2020-07-21 13:59:51 +0700

    Update ThreeSum.swift

--- Day2-Three-Sum/ThreeSum.swift
+++ Day2-Three-Sum/ThreeSum.swift
@@ -1,12 +1,12 @@
 
-//## 3Sum
+//ThreeSum
 //
-//> Given an array of integers, find all subsets of the array with 3 values where the 3 values sum up to a target number.
-//>
-//> **Note**: The solution subsets must not contain duplicate triplets.
-//>
-//> For example, given the array [-1, 0, 1, 2, -1, -4], and the target **0**:
-//> The solution set is: [[-1, 0, 1], [-1, -1, 2]] // The two **-1** values in the array are considered to be distinct
+//Given an array of integers, find all subsets of the array with 3 values where the 3 values sum up to a target number.
+//
+//**Note**: The solution subsets must not contain duplicate triplets.
+//
+//For example, given the array [-1, 0, 1, 2, -1, -4], and the target **0**:
+//The solution set is: [[-1, 0, 1], [-1, -1, 2]] // The two **-1** values in the array are considered to be distinct
 //
 //There are 2 key procedures in solving this algorithm. Sorting the array, and avoiding duplicates.
 
